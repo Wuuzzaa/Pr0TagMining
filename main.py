0e3c3139a98c42920706ee63c8e423cbb5a8607d
Wuuzzaa <jonas-licht@gmx.de>
2018-08-26 21:02:39 +0200

    Error Handling hinzugefügt.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as Bs
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import re
+import time
 
 
 def create_driver(CSS_BLOCK, IMAGES_BLOCK):
@@ -130,6 +131,11 @@ def get_site_soup(url):
 
 
 def print_data_programm_new(new_id):
+    """
+    Gibt alle Daten zu einem Post aus (Benis, Uploaddatum, Uploader, Tags...)
+    :param new_id:
+    :return: Success 0, NOT SFW 1, Image not there 2, Server error 503
+    """
     soup = get_site_soup("https://pr0gramm.com/new/" + str(new_id)).prettify()
 
     # prüfen ob Bild in SFW ist sonst Abbruch
@@ -137,14 +143,28 @@ def print_data_programm_new(new_id):
         print("https://pr0gramm.com/new/" + str(new_id))
         print()
         print("Bild ist nicht SFW!")
-        return
+        return 1
 
     # prüfen ob Bild vorhanden ist
     if "Nichts gefunden ¯\_(ツ)_/¯" in soup:
         print("https://pr0gramm.com/new/" + str(new_id))
         print()
         print("Bild ist nicht mehr verfügbar/gelöscht")
-        return
+        return 2
+
+    # Prüfen ob Server korrekt antwortet 503 Error abfangen
+    if "503 Service Temporarily Unavailable" in soup:
+        print("https://pr0gramm.com/new/" + str(new_id))
+        print()
+        print("503 ERROR")
+        return 503
+
+    # ZOMFG Error abfangen
+    if "Irgendwas Doofes ist passiert. Probier's später nochmal" in soup:
+        print("https://pr0gramm.com/new/" + str(new_id))
+        print()
+        print("ZOMFG ERROR")
+        return 503
 
     benis = get_benis(soup)
     time = get_upload_datum(soup)
@@ -162,14 +182,19 @@ def print_data_programm_new(new_id):
     print("Bad Tags: ")
     print(tags_bad)
 
-driver = create_driver(True, True)
+    return 0
+
+driver = create_driver(False, False)
 
 start = timeit.default_timer()
 
 for i in range(1, 51):
-    print_data_programm_new(i)
+    error = print_data_programm_new(i)
 
-
+    # On Server-error cause of too many request by this programm wait a few seconds
+    while error == 503:
+        time.sleep(10)
+        error = print_data_programm_new(i)
 
 driver.close()
 
